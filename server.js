f0f2ad8d3cab067841b9670383057ab11c3054c5
Kornelija Laucius <kornelija.janusaite@gmail.com>
2020-12-05 14:11:34 -0600

    product by id route working

--- server.js
+++ server.js
@@ -40,6 +40,43 @@ app.get('/products', async (req, res) => {
 
 })
 
+app.get('/products/:product_id', async (req, res) => {
+
+  let productId = req.params.product_id;
+
+  let productByIDQuery = `SELECT p.id,
+                            p.name,
+                            slogan,
+                            description,
+                            c.name as category,
+                            default_price
+                          FROM sdc.products p
+                          JOIN sdc.categories c on c.id = p.category_id
+                          WHERE p.id = ` + productId;
+
+   let productFeaturesByIdQuery = `SELECT feature,
+                                    value
+                                  FROM sdc.features f
+                                  JOIN sdc.product_feature_map pf on f.id = pf.feature_id
+                                  WHERE pf.product_id = ` + productId;
+
+  const productsResult = await pool.query(productByIDQuery);
+  const featureResult = await pool.query(productFeaturesByIdQuery);
+
+  let productWithFeatures = {
+    id: productsResult.rows[0].id,
+    name: productsResult.rows[0].name,
+    slogan: productsResult.rows[0].slogan,
+    description: productsResult.rows[0].description,
+    category: productsResult.rows[0].category,
+    default_price: productsResult.rows[0].default_price,
+    features: featureResult.rows
+  }
+
+  res.send(productWithFeatures);
+
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
